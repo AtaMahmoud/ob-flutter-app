8f8f67d6fdc924afbe7c0c6363e3db752eea62a9
Abdullah <info.alasad@gmail.com>
2020-11-30 00:03:51 +0600

    adds theme colors

---
+++ lib/core/common_widgets/common_theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+
+class CommonTheme{
+  CommonTheme._();
+  static const Color primaryLightest = Color(0xFFE4EDF3);
+  static const Color primaryLighter = Color(0xFF9CC4F4);
+  static const Color primaryLight = Color(0xFF649FF8);
+  static const Color primary = Color(0xFF2D68BF);
+  static const Color primaryDark = Color(0xFF224C8C);
+  static const Color primaryDarker = Color(0xFF081C5B);
+  static const Color primaryDarkest = Color(0xFFFFFFFF);
+
+  static final Color secondaryLightest = Color(0xFFFFFFFF).withOpacity(0.3);
+  static final Color secondaryLighter = Color(0xFFFFFFFF).withOpacity(0.3);
+  static final Color secondaryLight = Color(0xFFFFFFFF).withOpacity(0.3);
+  static final Color secondary = Color(0xFFFFFFFF).withOpacity(0.3);
+  static final Color secondaryDark = Color(0xFFFFFFFF).withOpacity(0.3);
+  static final Color secondaryDarker = Color(0xFFFFFFFF).withOpacity(0.3);
+  static final Color secondaryDarkest = Color(0xFFFFFFFF).withOpacity(0.3);
+
+  static const Color greyLightest = Color(0xFFF7F7F7);
+  
+
+}
