11e0cc6302eebc1c09b465d643ee063730f76442
Asad <68988943+pongta@users.noreply.github.com>
2021-03-20 23:02:29 +0600

    adds local notifications and fcm

---
+++ android/app/src/main/java/com/ss/ocean_builder/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ss.oceanbuilders;
+import io.flutter.embedding.android.FlutterActivity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.media.RingtoneManager;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import java.util.*;
+
+public class MainActivity extends FlutterActivity {
+
+
+}
